e8633d454172e68902ea02edf6e1063886fc5120
Craig R Morton <crmpicco@aol.co.uk>
2020-02-07 09:56:58 +0800

    Pull the TELEGRAM_CRMPICCO and TELEGRAM_API_KEY environment variables in

--- rangers-lotto.py
+++ rangers-lotto.py
@@ -10,21 +10,20 @@ import requests
 import re
 from pprint import pprint
 
-# @TODO pull from environment variables, e.g os.getenv('TELEGRAM_CRMPICCO'), os.environ['TELEGRAM_APIKEY']
-telegram = TelegramBot()
-telegram_recipient = 1872
+telegram = TelegramBot(os.environ['TELEGRAM_API_KEY'])
+telegram_recipient = os.getenv('TELEGRAM_CRMPICCO')
 
-# @TODO change this URL to the latest results page
-page = requests.get("http://www.rangerslotto.co.uk/?page_id=82&lottoId=27")
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-balls = []
-for entry_content in soup.find_all('img',vspace='12'):
-    balls.append(entry_content['src'].rsplit('/', 1)[-1].split('.')[0])
-
-winning_numbers = ' ' . join(balls)
-print 'The winning numbers were ' + winning_numbers
+# # @TODO change this URL to the latest results page
+# page = requests.get("http://www.rangerslotto.co.uk/?page_id=82&lottoId=27")
+#
+# soup = BeautifulSoup(page.content, 'html.parser')
+#
+# balls = []
+# for entry_content in soup.find_all('img',vspace='12'):
+#     balls.append(entry_content['src'].rsplit('/', 1)[-1].split('.')[0])
+#
+# winning_numbers = ' ' . join(balls)
+# print 'The winning numbers were ' + winning_numbers
 
 # @TODO compare with my numbers
 
