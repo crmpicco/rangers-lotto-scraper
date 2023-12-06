50085c1dae2f2dfdf03013628765bac977e022c6
Craig R Morton <crmpicco@aol.co.uk>
2023-12-06 09:12:43 +0800

    Adding crontab entry

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 
 # Rangers Lotto Results Scraper
 Scrape the latest results from the Rangers Lotto site
+
+[![python](https://img.shields.io/badge/Python-3.9-3776AB.svg?style=flat&logo=python&logoColor=white)](https://www.python.org)
+
+## Crontab
+Add it to the crontab to run twice a week, for example
+```commandline
+# Post the Rangers Lotto results to Twitter twice per week
+15 17 * * 1,4 source /opt/virtualenv/rangers-lotto/bin/activate && /opt/virtualenv/rangers-lotto/bin/python3 /path/to/script/rangers-lotto-scraper/rangers-lotto.py > /var/log/rangers-lotto-scraper.log 2>&1
+```
--- rangers-lotto.py
+++ rangers-lotto.py
@@ -30,6 +30,7 @@ def post_to_twitter(message):
 def get_first_week_lottery_results(url):
     # Send a GET request to the URL
     response = requests.get(url)
+    print(f"Getting results from {url}")
 
     # Check if the request was successful (status code 200)
     if response.status_code != 200:
@@ -64,41 +65,43 @@ def get_first_week_lottery_results(url):
 
     results_element = week_soup.find("div", class_="entry-content")
 
-    if results_element:
-        # Find all images within the entry content
-        ball_images = results_element.find_all("img")
-        date_pattern = re.compile(r'\b(\w{3} \d{1,2}(?:st|nd|rd|th) \w+ \d{4})\b')
-        matches = list(date_pattern.finditer(results_element.get_text()))
-        # print(matches)
-        date_values = [match.group(0) for match in matches]
-        # print(date_values)
-        if ball_images:
-            # Extract and print the lottery ball numbers
-            ball_numbers = [image['src'].split('/')[-1].split('.')[0] for image in ball_images]
-
-            result_dict = {}
-            # Iterate over the dates and assign the first 5 elements of numbers to each date
-            for date, number_list in zip(date_values,
-                                         [ball_numbers[i:i + 5] for i in range(0, len(ball_numbers), 5)]):
-                result_dict[date] = number_list
-
-            twitter_message = 'Rangers Lotto Results 🔴⚪🔵\n'
-            for date, number_list in result_dict.items():
-                formatted_numbers = [f"Bonus: {num[5:]}" if num.startswith("bonus") else num for num in
-                                     number_list]
-                twitter_message += f"{date}\n"
-                twitter_message += f"{', '.join(formatted_numbers)}"
-                print(twitter_message)
-                try:
-                    post_to_twitter(twitter_message)
-                except Exception as e:
-                    print(f"There was a problem posting to Twitter - {e}")
-
-        else:
-            print("No lottery ball images found for the specified week.")
-    else:
+    if not results_element:
         print("No entry content found on the linked page.")
+
+    # Find all images within the entry content
+    ball_images = results_element.find_all("img")
+    date_pattern = re.compile(r'\b(\w{3} \d{1,2}(?:st|nd|rd|th) \w+ \d{4})\b')
+    matches = list(date_pattern.finditer(results_element.get_text()))
+    # print(matches)
+    date_values = [match.group(0) for match in matches]
+    # print(date_values)
+    if not ball_images:
+        print("No lottery ball images found for the specified week.")
+
+    # Extract and print the lottery ball numbers
+    ball_numbers = [image['src'].split('/')[-1].split('.')[0] for image in ball_images]
+
+    result_dict = {}
+    # Iterate over the dates and assign the first 5 elements of numbers to each date
+    for date, number_list in zip(date_values,
+                                 [ball_numbers[i:i + 5] for i in range(0, len(ball_numbers), 5)]):
+        result_dict[date] = number_list
+
+    twitter_message = 'Rangers Lotto Results 🔴⚪🔵\n'
+    for date, number_list in result_dict.items():
+        formatted_numbers = [f"Bonus: {num[5:]}" if num.startswith("bonus") else num for num in
+                             number_list]
+        twitter_message += f"{date}\n"
+        twitter_message += f"{', '.join(formatted_numbers)}"
+        print(twitter_message)
+        try:
+            post_to_twitter(twitter_message)
+        except Exception as e:
+            print(f"There was a problem posting to Twitter - {e}")
 
 
 # Call the function to get and parse lottery results for the first "Week" link
-get_first_week_lottery_results(url)
+try:
+    get_first_week_lottery_results(url)
+except Exception as e:
+    print(f"Exception thrown when getting Rangers Lotto numbers - {e}")
